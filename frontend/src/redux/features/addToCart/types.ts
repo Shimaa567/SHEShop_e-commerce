37cb078006764042bf160e86729594cd2930d5a3
Mohammed Essam <dev.mohammed.essam@gmial.com>
2021-03-05 01:14:50 +0200

    changing cart item type to the right type

--- frontend/src/redux/features/addToCart/types.ts
+++ frontend/src/redux/features/addToCart/types.ts
@@ -1,28 +1,26 @@
 //import { Product } from '../../../types/Product';
 
-import { Product } from '../../../types/Product';
+import { Product } from "../../../types/Product";
 
-export const CART_ADD_ITEM = 'CART_ADD_ITEM';
-export const CART_REMOVE_ITEM = 'CART_REMOVE_ITEM';
+export const CART_ADD_ITEM = "CART_ADD_ITEM";
+export const CART_REMOVE_ITEM = "CART_REMOVE_ITEM";
 
 export interface CartState {
   //product?: Product;
-  cartItems: Array<{ product: string }>;
+  cartItems: Array<CartItem>;
 }
-// export interface CartItem {
-//   product?: Product;
-//   //cartItems?: Array<string>
-// }
+export interface CartItem {
+  product: string | number;
+  name?: string;
+  image?: string;
+  price?: string | number;
+  countInStock?: number;
+  qty?: number;
+}
 
 export interface AddToCart {
   type: typeof CART_ADD_ITEM;
-  payload: {
-    product: string | number;
-    name: string;
-    image: string;
-    price: string | number;
-    countInStock: number;
-  };
+  payload: CartItem;
 }
 
 export interface RemoveFromCart {
