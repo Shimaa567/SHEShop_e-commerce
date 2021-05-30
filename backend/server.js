f25c65554e72e728f92485760056cae0eb9fd6e8
Shimaa567 <shimaaalsersy@gmail.com>
2021-05-30 14:29:59 +0200

    Prepare for deploy

--- backend/server.js
+++ backend/server.js
@@ -43,7 +43,7 @@ if (process.env.NODE_ENV === "production") {
   );
 } else {
   app.get("/", (req, res) => {
-    res.send("New API is running....");
+    res.send("New API is running......");
   });
 }
 
